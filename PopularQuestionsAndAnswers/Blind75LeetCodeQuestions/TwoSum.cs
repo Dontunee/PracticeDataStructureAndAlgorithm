a209828611f4967ed988b18dc7b86fac2756d328
Babatunde Afolabi <t_afolabi@yahoo.com>
2022-01-10 22:23:22 +0100

    Complete two sum solutionbrute force and optimized approach

---
+++ PopularQuestionsAndAnswers/Blind75LeetCodeQuestions/TwoSum.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopularQuestionsAndAnswers.Blind75LeetCodeQuestions
+{
+    public static class TwoSum
+    {
+        //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        //You can return the answer in any order.
+        public static int[] TwoSumBruteForceSolution(int[] nums, int target)
+        {
+            int numsLength = nums.Length;
+            if (numsLength < 1)
+                throw new ArgumentOutOfRangeException("input array must have numbers");
+
+            for(int i = 0; i < numsLength; i++)
+            {
+                for(int j = 1; j < numsLength - 2; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+
+
+        public static int[] TwoSumDictionarySolution(int[] nums, int target)
+        {
+            if (nums.Length < 2 || nums.Length >= 104)
+                if (target <= -Math.Pow(10,9) ||target >= Math.Pow(10,9))
+                    throw new ArgumentOutOfRangeException("input numbers must be greater than 0");
+
+           
+            var dictionary = new Dictionary<int, int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] <= -Math.Pow(10,9) || nums[i] >= Math.Pow(10,9))
+                    throw new ArgumentOutOfRangeException("input number must be greater than 0");
+
+                var remainderTarget = target - nums[i];
+                if(dictionary.ContainsKey(nums[i]))
+                {
+                    return new int[] { dictionary.GetValueOrDefault(nums[i]), i };
+                }
+                dictionary.TryAdd(remainderTarget, i);
+            }
+
+            return new int[0] { };
+        }
+
+
+    }
+}
---
+++ Tests/TwoSum_Tests.cs
@@ -0,0 +1,32 @@
+﻿using PopularQuestionsAndAnswers.Blind75LeetCodeQuestions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class TwoSum_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 },9)]
+        public void BruteForceSolution_ValidArguments_ReturnsOutput(int[] numbers,int target)
+        {
+            var response = TwoSum.TwoSumBruteForceSolution(numbers, target);
+            Assert.Equal(0, response[0]);
+            Assert.Equal(1, response[1]);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9)]
+        public void TwoSumDictionarySolution_ValidArguments_ReturnsOutput(int[] numbers, int target)
+        {
+            var response = TwoSum.TwoSumDictionarySolution(numbers, target);
+            Assert.Equal(0, response[0]);
+            Assert.Equal(1, response[1]);
+        }
+
+    }
+}
