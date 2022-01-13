c2d6c94d497744188cb4793390a583dfdb62a384
Babatunde Afolabi <t_afolabi@yahoo.com>
2022-01-13 09:14:21 +0100

    implement solution for contains duplicate

---
+++ PopularQuestionsAndAnswers/Blind75LeetCodeQuestions/ContainsDuplicate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopularQuestionsAndAnswers.Blind75LeetCodeQuestions
+{
+    public static class ContainsDuplicate
+    {
+        // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+        public static bool ContainsDuplicateSolution(int[] nums)
+        {
+            if (nums.Length < 1 || nums.Length > Math.Pow(10, 5))
+                return false;
+            var hashSet = new HashSet<int>();
+            foreach (var item in nums)
+            {
+                if (item < -Math.Pow(10, 9) || item > Math.Pow(10, 9))
+                    return false;
+                if(hashSet.Contains(item))
+                {
+                    return true;
+                }
+                else
+                {
+                    hashSet.Add(item);
+                }
+            }
+            return false;
+
+        }
+
+    }
+}
---
+++ Tests/ContainsDuplicate_Tests.cs
@@ -0,0 +1,22 @@
+﻿using PopularQuestionsAndAnswers.Blind75LeetCodeQuestions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class ContainsDuplicate_Tests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 1})]
+        public void ContainsDuplicateSolution_ValidInput_ValidSolution(int[] numbers)
+        {
+            var response = ContainsDuplicate.ContainsDuplicateSolution(numbers);
+            Assert.True(response == true);
+        }
+    }
+}
