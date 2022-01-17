54ef0d2a09738966d2f3e626d26f0cedcea38dfe
Babatunde Afolabi <t_afolabi@yahoo.com>
2022-01-17 11:18:08 +0100

    solve maximum subarray

---
+++ PopularQuestionsAndAnswers/Blind75LeetCodeQuestions/MaximumSubarray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopularQuestionsAndAnswers.Blind75LeetCodeQuestions
+{
+    public static class MaximumSubarray
+    {
+
+        public static int MaxSubArray(int[] nums)
+        {
+            int currentSum = nums[0];
+            int maximumSum = int.MinValue;
+
+            for(int i = 1; i < nums.Length; i++) { 
+
+                if (currentSum < 0)
+                {
+                    currentSum = 0;
+                }
+                currentSum += nums[i];
+                maximumSum = Math.Max(maximumSum, currentSum);
+            }
+            return maximumSum;
+        }
+    }
+}
---
+++ Tests/MaxSubArray_Tests.cs
@@ -0,0 +1,21 @@
+﻿using PopularQuestionsAndAnswers.Blind75LeetCodeQuestions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class MaxSubArray_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { -2,1,-3,4,-1,2,1,-5,4})]
+        public void MaxSubArray_ValidInput_ReturnsValidOutput(int[] nums)
+        {
+            var result = MaximumSubarray.MaxSubArray(nums);
+            Assert.Equal("6", result.ToString());
+        }
+    }
+}
